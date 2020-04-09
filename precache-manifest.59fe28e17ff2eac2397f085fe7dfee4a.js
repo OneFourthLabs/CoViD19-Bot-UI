212ee43572e6c27e6c476086a242a54bac431eb4
Avik <avik@onefourthlabs.com>
2020-04-09 14:10:56 +0530

    Deploy

---
+++ precache-manifest.59fe28e17ff2eac2397f085fe7dfee4a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf4ca9a621f009bf069",
+    "url": "/CoViD19-Bot-UI/css/app.003ec0ce.css"
+  },
+  {
+    "revision": "d0df3c8acc58d6734e86",
+    "url": "/CoViD19-Bot-UI/css/chunk-vendors.4304d73c.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/CoViD19-Bot-UI/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/CoViD19-Bot-UI/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "6f69d99b9b0706a2a955ed42d64742a1",
+    "url": "/CoViD19-Bot-UI/fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-500.6f69d99b.woff2"
+  },
+  {
+    "revision": "851a2b5a8394eb1b868678bfd31a1a8a",
+    "url": "/CoViD19-Bot-UI/fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-500.851a2b5a.woff"
+  },
+  {
+    "revision": "9549360090baf2eb8b25d3a9708fc19d",
+    "url": "/CoViD19-Bot-UI/fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-regular.95493600.woff2"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/CoViD19-Bot-UI/fonts/roboto-v20-greek-ext_cyrillic-ext_greek_latin-ext_latin_cyrillic_vietnamese-regular.a91ad097.woff"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "/CoViD19-Bot-UI/img/opengraph.png"
+  },
+  {
+    "revision": "6b3bc75f6301f0caedf6394255f59924",
+    "url": "/CoViD19-Bot-UI/index.html"
+  },
+  {
+    "revision": "bdf4ca9a621f009bf069",
+    "url": "/CoViD19-Bot-UI/js/app.34aa6d6a.js"
+  },
+  {
+    "revision": "d0df3c8acc58d6734e86",
+    "url": "/CoViD19-Bot-UI/js/chunk-vendors.3a5b3b94.js"
+  },
+  {
+    "revision": "4e9e378d9198b35b8f10f6ca6e59e55e",
+    "url": "/CoViD19-Bot-UI/manifest.json"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "/CoViD19-Bot-UI/robots.txt"
+  }
+]);
